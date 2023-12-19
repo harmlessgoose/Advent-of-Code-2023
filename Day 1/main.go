ca4649ecb2e44cc42194e3f2b1ef80e76174fdf2
Peter O <65027464+harmlessgoose@users.noreply.github.com>
2023-12-19 15:16:55 +0000

    Loop through array, add to total

--- Day 1/main.go
+++ Day 1/main.go
@@ -5,47 +5,78 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	// Import text file
-    input, err := os.Open("input.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer input.Close()
+	total := 0
 
-    scanner := bufio.NewScanner(input)
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	// Import text file
+	input, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer input.Close()
+
+	scanner := bufio.NewScanner(input)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
 	var codes []string
 
 	// Put each line into an array
 	for scanner.Scan() {
 		codes = append(codes, scanner.Text())
-    }
-
-	// Loop through array
-	for i, code := range codes {
-		fmt.Println(i, code)
 	}
 
+	// Loop through array
+	for _, code := range codes {
 
+		firstDigit := ""
+		secondDigit := ""
 
+		// From the front, find the first number
+		for _, char := range code {
+			char := string(char)
 
+			if isNumber(char) {
+				firstDigit = char
+				break
+			}
+		}
 
-	// From the front, find the first number
+		// From the back, find the second number
 
-	// From the back, find the second number
+		for i := len(code) - 1; i >= 0; i-- {
+			char := string(code[i])
+			if isNumber(char) {
+				secondDigit = char
+				break
+			}
+		}
 
-	// Combine the digits as strings
+		// Combine the digits as strings
 
-	// Cast to int
+		number := firstDigit + secondDigit
 
-	// Add to total
+		// Cast to int
+		num, err := strconv.Atoi(number)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// Return total
+		// Add to total
+		total += num
 
+	}
+	fmt.Printf("Total: %d\n", total)
+}
+
+func isNumber(char string) bool {
+	if char == "0" || char == "1" || char == "2" || char == "3" || char == "4" || char == "5" || char == "6" || char == "7" || char == "8" || char == "9" {
+		return true
+	}
+
+	return false
 }
