f50f4cff6c36864efd8651884cbb0ab8c8f2ea63
Peter O <65027464+harmlessgoose@users.noreply.github.com>
2023-12-21 15:24:38 +0000

    Finished

--- Day 2/main.go
+++ Day 2/main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	var games []string
+	idTotal := 0
+
 	// Import input file
 	input, err := os.Open("input.txt")
 	if err != nil {
@@ -21,31 +25,134 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var games []string
-
-	// Put each line into an array
+	// Put each line from the input into an array
 	for scanner.Scan() {
 		games = append(games, scanner.Text())
 	}
 
-	// Loop through array
-	for _, game := range games {
+	// Loop through games
+	for i, game := range games {
 
 		// Split into subgames
-		subGame := strings.Split(game, ";")
+		subGames := strings.Split(game, ";")
 
-		fmt.Println(subGame[0])
+		highestRed := 0
+		highestGreen := 0
+		highestBlue := 0
+		isPossible := true
+		gameId := i + 1
 
-		// For each line, scan for each word
+		fmt.Printf("Game ID: %d\n", gameId)
 
-		// If the word is a colour, extract the number
+		// For each subgame, extract each colour
+		for _, subgame := range subGames {
 
-		// Add the number to a running total
+			red := getRed(subgame)
+			if red > highestRed {
+				highestRed = red
+			}
+			green := getGreen(subgame)
+			if green > highestGreen {
+				highestGreen = green
+			}
+			blue := getBlue(subgame)
+			if blue > highestBlue {
+				highestBlue = blue
+			}
+		}
 
 		// Based on the total, decide if the game is valid
+		if highestRed > 12 || highestGreen > 13 || highestBlue > 14 {
+			isPossible = false
+		}
 
-		// If so, extract the game ID and add to the total
+		if isPossible {
+			idTotal += gameId
+		}
+
+		fmt.Printf("Highest Red is %d\n", highestRed)
+		fmt.Printf("Highest Green is %d\n", highestGreen)
+		fmt.Printf("Highest Blue is %d\n", highestBlue)
+		fmt.Printf("Game is possible: %t\n", isPossible)
+		fmt.Printf("\n")
 
 	}
+
 	// Return the total
+	fmt.Printf("Sum of all IDs is %d\n", idTotal)
+}
+
+func getRed(subGame string) int {
+	total := 0
+
+	// Get index of appearance of the word
+	redWordIndex := strings.Index(subGame, "red")
+
+	// Extract the value
+	if redWordIndex > 0 {
+		redWordValue := string(subGame[redWordIndex-3]) + string(subGame[redWordIndex-2])
+
+		// Trim whitespace
+		redWordValue = strings.TrimSpace(redWordValue)
+
+		// Cast to int
+		red, err := strconv.Atoi(redWordValue)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		total += red
+	}
+
+	return total
+}
+
+func getGreen(subGame string) int {
+	total := 0
+
+	// Get index of appearance of the word
+	greenWordIndex := strings.Index(subGame, "green")
+
+	// Extract the value
+	if greenWordIndex > 0 {
+		greenWordValue := string(subGame[greenWordIndex-3]) + string(subGame[greenWordIndex-2])
+
+		// Trim whitespace
+		greenWordValue = strings.TrimSpace(greenWordValue)
+
+		// Cast to int
+		green, err := strconv.Atoi(greenWordValue)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		total += green
+	}
+
+	return total
+}
+
+func getBlue(subGame string) int {
+	total := 0
+
+	// Get index of appearance of the word
+	blueWordIndex := strings.Index(subGame, "blue")
+
+	// Extract the value
+	if blueWordIndex > 0 {
+		blueWordValue := string(subGame[blueWordIndex-3]) + string(subGame[blueWordIndex-2])
+
+		// Trim whitespace
+		blueWordValue = strings.TrimSpace(blueWordValue)
+
+		// Cast to int
+		blue, err := strconv.Atoi(blueWordValue)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		total += blue
+	}
+
+	return total
 }
